a63a7ec11ff5ad93ae99d64e93adfa879c4a8d80
Kumar Kankam <112927086+itzkumar07@users.noreply.github.com>
2025-05-08 21:33:39 +0530

    Init Spring Boot project with base structure

---
+++ src/test/java/com/bootcoding/eCommerce_App/ECommerceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcoding.eCommerce_App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
